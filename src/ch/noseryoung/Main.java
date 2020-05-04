67824655010aefb413b2a06ae3df44fc4a2204f7
flxsch <felix.schneider@edu.tbz.ch>
2020-05-04 17:11:39 +0200

    updated list shows -> better formatting added table printer class

--- src/ch/noseryoung/Main.java
+++ src/ch/noseryoung/Main.java
@@ -1,8 +1,7 @@
 package ch.noseryoung;
 
+import javax.xml.transform.Result;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -31,28 +30,88 @@ public class Main {
   }
 
   public static void Menu(Connection connection) {
-    System.out.println(
-        "Menu:\n"
-            + "  [1] List shows\n"
-            + "  [2] Add new show\n"
-            + "  [3] Book show\n"
-            + "  [4] Delete show\n");
-    int choice = scan.nextInt();
+    int choice;
+    do {
+      while (true) {
+        try {
+          System.out.println(
+              "-------------------------\n"
+                  + "| Cinema Booking System |\n"
+                  + "-------------------------\n\n"
+                  + "  [1] List movies\n"
+                  + "  [2] Add movie\n"
+                  + "  [3] Book show\n"
+                  + "  [4] Delete movie\n"
+                  + "  [5] Exit\n");
+          choice = Integer.parseInt(scan.nextLine());
+        } catch (NumberFormatException e) {
+          System.out.println("el hijo de puta");
+          continue;
+        }
+        break;
+      }
 
-    switch (choice) {
-      case 1:
-        listShows(connection);
-        break;
-      case 2:
+      switch (choice) {
+        case 1:
+          listMovies(connection);
+          break;
+        case 2:
+          addMovie(connection);
+          break;
+        case 3:
+          bookShow();
+          break;
+        case 4:
+          deleteMovie(connection);
+          break;
+        case 5:
+          exit(connection);
+          break;
+        default:
+          break;
+      }
+      System.out.print("\n\n\n");
+    } while (choice != 5);
+  }
 
-        break;
-      case 3:
-        break;
-      case 4:
-        break;
-      default:
-        break;
+  public static void listMovies(Connection connection) {
+    try {
+      // create a statement
+      Statement statement = connection.createStatement();
+      // execute SQL statement
+      String sql_query =
+          "SELECT m.ID_Movie, m.MovieTitle, g.GenreType, d.DirectorName FROM Movie m "
+              + "JOIN Genre g "
+              + "ON m.Genre_ID = g.ID_Genre "
+              + "JOIN Director d "
+              + "ON m.Director_ID = d.ID_Director ORDER BY ID_Movie ASC;";
+      ResultSet result_set = statement.executeQuery(sql_query);
+
+      TablePrinter tablePrinter = new TablePrinter();
+      tablePrinter.setHeaders("Nr.", "Movie title", "Genre type", "Director name");
+
+      // process the result set
+      while (result_set.next()) {
+        String id_movie = result_set.getString("ID_Movie");
+        String movie_title = result_set.getString("MovieTitle");
+        String genre_type = result_set.getString("GenreType");
+        String director_name = result_set.getString("DirectorName");
+        tablePrinter.addRow(id_movie, movie_title, genre_type, director_name);
+      }
+      tablePrinter.print();
+    } catch (Exception e) {
+      e.printStackTrace(System.err);
     }
+  }
+
+  public static void addMovie(Connection connection) {}
+
+  public static void bookShow() {}
+
+  public static void deleteMovie(Connection connection) {}
+
+  public static void exit(Connection connection) {
+    System.out.println("Good bye, have a nice one!");
     if (connection != null) {
       try {
         connection.close();
@@ -61,35 +120,4 @@ public class Main {
       connection = null;
     }
   }
-
-  public static void listShows(Connection connection) {
-    try {
-      // create a statement
-      Statement stmt = connection.createStatement();
-      // execute SQL statement
-      String sqlQuery =
-          "SELECT m.MovieTitle, g.GenreType, d.DirectorName from Movie m "
-              + "join Genre g "
-              + "ON m.Genre_ID = g.ID_Genre "
-              + "join Director d "
-              + "ON m.Director_ID = d.ID_Director;";
-      ResultSet rs = stmt.executeQuery(sqlQuery);
-      // process the result set
-      while (rs.next()) {
-        String movie_title = rs.getString("MovieTitle");
-        String genre_type = rs.getString("GenreType");
-        String director_name = rs.getString("DirectorName");
-
-        System.out.println("" + movie_title + "" + genre_type + " " + director_name + "");
-      }
-    } catch (Exception e) {
-      e.printStackTrace(System.err);
-    }
-  }
-
-  public static void addShows(Connection connection){
-
-  };
-
-  void bookShow() {}
 }
