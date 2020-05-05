a4385865b529e7c3f54d5f1895ecd96ee8d73fd6
flxsch <felix.schneider@edu.tbz.ch>
2020-05-05 11:36:04 +0200

    CRUD Movie

--- src/ch/noseryoung/Main.java
+++ src/ch/noseryoung/Main.java
@@ -2,7 +2,7 @@ package ch.noseryoung;
 
 import javax.xml.transform.Result;
 import java.sql.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
   static Scanner scan = new Scanner(System.in);
@@ -104,11 +104,114 @@ public class Main {
     }
   }
 
-  public static void addMovie(Connection connection) {}
+  public static int getGenreID(Connection connection, String genre_type) {
+    Map<String, Integer> genres = new HashMap<>();
+
+    int genre_id = 0;
+    System.out.println(genre_type);
+    try {
+      Statement statement = connection.createStatement();
+      // execute SQL statement
+      ResultSet result_set = statement.executeQuery("SELECT * FROM Genre;");
+      while (result_set.next()) {
+        genres.put(result_set.getString("GenreType"), result_set.getInt("ID_Genre"));
+      }
+      if (genres.containsKey(genre_type)) {
+        genre_id = genres.get(genre_type);
+      } else {
+        statement.executeUpdate(
+            "INSERT INTO Genre (GenreType) VALUES (\'" + genre_type + "\');");
+        for(int x : genres.values()){
+          int temp = 0;
+          if(x > temp){
+            temp = x;
+          }
+          genre_id = temp;
+        }
+        }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return genre_id;
+  }
+
+  public static int getDirectorID(Connection connection, String director_name) {
+    Map<String, Integer> directors = new HashMap<>();
+    int director_id = 0;
+    try {
+      Statement statement = connection.createStatement();
+      // execute SQL statement
+      ResultSet result_set = statement.executeQuery("SELECT * FROM Director;");
+      while (result_set.next()) {
+        directors.put(result_set.getString("DirectorName"), result_set.getInt("ID_Director"));
+      }
+      if (directors.containsKey(director_name)) {
+        director_id = directors.get(director_name);
+      } else {
+        statement.executeUpdate(
+                "INSERT INTO Director (DirectorName) VALUES (\'" + director_name + "\');");
+        for(int x : directors.values()){
+          int temp = 0;
+          if(x > temp){
+            temp = x;
+          }
+          director_id = temp;
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return director_id;
+  }
+
+  public static void addMovie(Connection connection) {
+    try {
+      System.out.println("Movie title: ");
+      String movie_title = scan.nextLine();
+      System.out.println("Movie play time in minutes:");
+      String play_time = scan.nextLine();
+      System.out.println("Genre type:");
+      String genre_type = scan.nextLine();
+      System.out.println("Director name");
+      String director_name = scan.nextLine();
+
+      // create a statement
+      Statement statement = connection.createStatement();
+
+      int genre_id = getGenreID(connection, genre_type);
+      int director_id = getDirectorID(connection, director_name);
+
+      statement.executeQuery(
+          "INSERT INTO Movie (MovieTitle, MovieTime, Genre_ID, Director_ID) VALUES (\'"
+              + movie_title
+              + "\',\'"
+              + play_time
+              + "\',\'"
+              + genre_id
+              + "\',\'"
+              + director_id
+              + "\');");
+
+    } catch (Exception e) {
+      e.printStackTrace(System.err);
+    }
+    System.out.println("Added successfully");
+  }
 
   public static void bookShow() {}
 
-  public static void deleteMovie(Connection connection) {}
+  public static void deleteMovie(Connection connection) {
+    System.out.println("Which movie do you want to delete?");
+    listMovies(connection);
+    int movie_id = Integer.parseInt(scan.nextLine());
+    try{
+      Statement statement = connection.createStatement();
+      // execute SQL statement
+      statement.executeUpdate("DELETE FROM Movie WHERE ID_Movie =\' " + movie_id + "\';");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
   public static void exit(Connection connection) {
     System.out.println("Good bye, have a nice one!");
